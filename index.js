4df87bb7b4d168a8934af26010a52a2cd8522b1b
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2021-12-04 18:41:47 +0100

    f

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-import * as fluent from "@fluentui/react";
-import * as spectum from "@adobe/react-spectrum";
-import * as antd from "antd";
-import * as moment from "moment";
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-console.log(fluent, spectum, antd, moment, React, ReactDOM);
-console.log(2);
+import * as x from "@fluentui/react/lib/components/ActivityItem/index.js";
+console.log(x)
